b713bffe1fb66729a780b76d3326228a1bbd22e4
AataMie <57095052+AataMie@users.noreply.github.com>
2021-09-07 11:12:43 +0200

    wip

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService} from 'app/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo;
+  sub: Subscription;
+  constructor(private _productService: ProductService ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.sub = this._productService.getAlbum(1).subscribe({
+      next: response => {
+        this.albumInfo = response} 
+    });
   }
 
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+private _albumUrl: string = '../assets/album.json';
+
+  constructor(private _http: HttpClient) { }
+
+  getAlbum(id: number){
+  return this._http.get(this._albumUrl)
+  }
+}
